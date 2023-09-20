468fd30fc85963182e3611a050bba78daa6a4f72
Asiaviscardi <asia.viscardii@gmail.com>
2023-09-20 18:48:52 +0200

    inserimento timer

--- script.js
+++ script.js
@@ -1,6 +1,32 @@
 /*numeri random*/
 
+
 function random() {
     num = Math.round(Math.random() * 100);
     document.getElementById("random").innerHTML = num;
 }
+
+
+
+/*timer*/
+
+window.onload = function() {
+    let secondi = 15;
+    setInterval(function() {
+
+        if(secondi > 0){
+            document.getElementById("timer").innerHTML = "Hai ancora" + secondi + "per memorizzare i numeri";
+            secondi--;
+            
+        }else{
+            document.getElementById("timer").innerHTML = "tempo scaduto";
+        }
+    }, 1000)
+}
+
+
+
+
+
+
+
